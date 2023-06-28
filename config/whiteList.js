98f9308691f71a96f96ae3e7aa7e44e38bec0055
metudein <maxmetadein@gmail.com>
2023-06-28 01:07:27 +0100

    update

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,11 +1,10 @@
 const whiteList = [
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://tritonsprime.io',
-    'https://tritonsprime.io',
-    'http://www.tritonsprime.io/',
-    'https://tritonsnftbroker.netlify.app/',
-    'https://trtionprime-app.onrender.com',
+    'http://app.tritonsprime.io',
+    'https://app.tritonsprime.io',
+    'http://www.app.tritonsprime.io/',
+    
 ]
 
 module.exports = whiteList
