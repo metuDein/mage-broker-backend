8b72916ac72863e2c0e3e4dba6ae5aeaf23f1e17
metuDein <119444184+metuDein@users.noreply.github.com>
2023-10-26 09:47:29 +0100

    Update whiteList.js

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,11 +1,15 @@
 const whiteList = [
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://app.tritonsprime.io',
-    'https://app.tritonsprime.io',
-    'http://www.app.tritonsprime.io/',
+//    'http://app.tritonsprime.io',
     'https://tritonsprime.com/',
-    'https://www.tritonsprime.com/'
+    'https://www.tritonsprime.com/',
+    'http://tritonsprime.com/',
+    'http://www.tritonsprime.com/'
+ //   'https://app.tritonsprime.io',
+  //  'http://www.app.tritonsprime.io/',
+  //  'https://tritonsprime.com/',
+ //   'https://www.tritonsprime.com/'
 ]
 
 module.exports = whiteList
